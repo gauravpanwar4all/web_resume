8108f3005e79e8e839965d4b6505ab981a279afb
gauravpanwar4all <62298353+gauravpanwar4all@users.noreply.github.com>
2020-03-23 20:55:20 +0530

    My web resume

---
+++ precache-manifest.2f215c2cdbc6d09e5a114e7ae52b6b6e.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db5416dda630f055322651c33e24c23",
+    "url": "/web_resume/index.html"
+  },
+  {
+    "revision": "90aa7a26dae313af3124",
+    "url": "/web_resume/static/css/main.05c50ecf.chunk.css"
+  },
+  {
+    "revision": "90528df232b613d9bf3b",
+    "url": "/web_resume/static/js/2.facc7c67.chunk.js"
+  },
+  {
+    "revision": "3aba3a90632230c98c06faa53d3a257f",
+    "url": "/web_resume/static/js/2.facc7c67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90aa7a26dae313af3124",
+    "url": "/web_resume/static/js/main.96ae0039.chunk.js"
+  },
+  {
+    "revision": "a611d05b280723f1751b",
+    "url": "/web_resume/static/js/runtime-main.eb5ea97d.js"
+  },
+  {
+    "revision": "edebc3561ed2b0d82c9682dcb8b8b79b",
+    "url": "/web_resume/static/media/appleclone.edebc356.jfif"
+  },
+  {
+    "revision": "bf28cefbaf2370100c941f11cf715cfb",
+    "url": "/web_resume/static/media/bcci_clone.bf28cefb.jfif"
+  },
+  {
+    "revision": "0c6fb74686569998e1d65468dfb41d06",
+    "url": "/web_resume/static/media/bookmyshow_clone.0c6fb746.jfif"
+  },
+  {
+    "revision": "0d0dffc8b920274fb20e8bfa4fe63fc3",
+    "url": "/web_resume/static/media/claro-agroAnalysis.0d0dffc8.jfif"
+  },
+  {
+    "revision": "95234b15c86ec0daa3d9e2b1df0f50c4",
+    "url": "/web_resume/static/media/claro-agroform.95234b15.jfif"
+  },
+  {
+    "revision": "a750a569957c4a6a7257e098a3fcd7b9",
+    "url": "/web_resume/static/media/claro-farmer.a750a569.jfif"
+  },
+  {
+    "revision": "ce4f60b14717599772a13d72d83302d8",
+    "url": "/web_resume/static/media/claro-hc.ce4f60b1.jfif"
+  },
+  {
+    "revision": "a704be982ca688924c79f9aa20e12d4e",
+    "url": "/web_resume/static/media/claro-home.a704be98.jfif"
+  },
+  {
+    "revision": "2b76d4be33ca3aa0c2a808079e95a8ff",
+    "url": "/web_resume/static/media/claro-iaas.2b76d4be.jfif"
+  },
+  {
+    "revision": "2fb7aa5ddf874a51cb101b903349f8fc",
+    "url": "/web_resume/static/media/claro-mandiDashboard.2fb7aa5d.jfif"
+  },
+  {
+    "revision": "3e0efe27763ae7d301b3f8f0a97f66e4",
+    "url": "/web_resume/static/media/claro-rms.3e0efe27.jfif"
+  },
+  {
+    "revision": "fcc22b8ff6d8f934a8eeafe87187fe4d",
+    "url": "/web_resume/static/media/entry.fcc22b8f.jfif"
+  },
+  {
+    "revision": "5d73efa1df8570de5eaad12253944168",
+    "url": "/web_resume/static/media/gaming.5d73efa1.jfif"
+  },
+  {
+    "revision": "af9d8de28861cf0a4d5a914ea5612cfc",
+    "url": "/web_resume/static/media/gaurav.af9d8de2.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web_resume/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5e4fe600f727574c9786cfe9a0912358",
+    "url": "/web_resume/static/media/ogt.5e4fe600.jfif"
+  },
+  {
+    "revision": "4871068fc80b7e7e953ef4215341c3a6",
+    "url": "/web_resume/static/media/vendor.4871068f.jfif"
+  },
+  {
+    "revision": "6641804f86c9ce20909abc5a2ac80af0",
+    "url": "/web_resume/static/media/wynkClone.6641804f.jfif"
+  }
+]);
